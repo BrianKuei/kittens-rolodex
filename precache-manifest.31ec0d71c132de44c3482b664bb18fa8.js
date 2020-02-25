eb065213faf9ca660459d451c7e760a3c21d622b
Kuei <vx3012564897@gmail.com>
2020-02-25 17:55:59 +0800

    Updates

---
+++ precache-manifest.31ec0d71c132de44c3482b664bb18fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76587317c820539e12a82ab9191f6861",
+    "url": "/kittens-rolodex/index.html"
+  },
+  {
+    "revision": "3fa607d54fe18a3685a5",
+    "url": "/kittens-rolodex/static/css/main.759d4f8b.chunk.css"
+  },
+  {
+    "revision": "9127540bb1a5c65f22ad",
+    "url": "/kittens-rolodex/static/js/2.d12247df.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kittens-rolodex/static/js/2.d12247df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa607d54fe18a3685a5",
+    "url": "/kittens-rolodex/static/js/main.244a6de5.chunk.js"
+  },
+  {
+    "revision": "69bd316e39a33d17b209",
+    "url": "/kittens-rolodex/static/js/runtime-main.945150ca.js"
+  }
+]);
